0135dcf6a9d2f3cb6e5f93718c6a2f48615f9e0f
Laura Meyer <laura-meyer@live.de>
2023-07-03 17:12:43 +0200

    finished some exercises

--- 01-Python/Python-for-data-science/data-environment/flask_option.py
+++ 01-Python/Python-for-data-science/data-environment/flask_option.py
@@ -4,7 +4,9 @@ import os
 
 def start():
     """returns the right message"""
-    pass  # YOUR CODE HERE
+    # YOUR CODE HERE
+    env = os.getenv(key = 'FLASK_ENV', default="empty")
+    return f"Starting in {env} mode..."
 
 if __name__ == "__main__":
     print(start())
