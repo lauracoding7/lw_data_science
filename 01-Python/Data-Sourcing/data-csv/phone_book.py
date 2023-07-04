22ec572867f7053c3ade94d880a7b5f8f1db092e
Laura Meyer <laura-meyer@live.de>
2023-07-04 11:23:33 +0200

    Started day 3

--- 01-Python/Data-Sourcing/data-csv/phone_book.py
+++ 01-Python/Data-Sourcing/data-csv/phone_book.py
@@ -1,1 +1,16 @@
+import csv
 
+with open('data/phone_book.csv', mode='r') as csv_file:
+    csv_reader = csv.reader(csv_file) # reader returns a list
+    line_count = 0
+    for row in csv_reader:
+        # ROW IS LIST
+        print(row)
+        line_count += 1
+
+with open('data/phone_book.csv', mode='r') as csv_file:
+    csv_reader = csv.DictReader(csv_file, skipinitialspace=True) # DictReader returns a dictionary
+
+    for row in csv_reader:
+        # ROW IS DICTIONARY
+        print(f"{row['last_name']}: {row['phone_number']}")
